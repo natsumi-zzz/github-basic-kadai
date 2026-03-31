45601b4e6e5ec65babea5c7605366fa29109b4d6
natsumi-zzz <iiil.7.lnatsumi@gmail.com>
2026-03-31 16:41:40 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重の代入（変数: weight）
+let weight = 68;
+
+// 身長の代入（変数: height）
+let height = 1.7;
+
+// BMIの計算（BMI = 体重 / (身長 * 身長)）
+const bmi = weight / (height * height);
+
+// コンソールへの出力
+console.log(bmi);
